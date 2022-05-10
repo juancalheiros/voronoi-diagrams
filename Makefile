7960d7a58587abaf2042c7fa651dfa6d4c327efa
Juan Calheiros <juan.calheiros@g.globo>
2022-05-10 20:41:30 -0300

    feat: added parameter to generate random points

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@ setup:
 	@python3 -m pip install -r requirements.txt
 
 run:
-	@python3 app.py --hosp=${hosp}
+	@python3 app.py --hosp=${hosp} --randon=${randon} --qnt_point=${qntd}
 
 .PHONY: setup
--- app.py
+++ app.py
@@ -6,25 +6,25 @@ Usage:
 
 Options:
     -d --data= <array of coordinates>
-    --hosp=<string>
+    --hosp=<bool>
+    --randon=<bool>
+    --qnt_point=<int>
 
 """
 
 from docopt import docopt
-
+import numpy as np
 from voronoi.settings import FULL_HOSPITAL, HOSPITAL
 from voronoi.diagrams_voronoi import diagrams_of_voronoi
 
 
 
-def main(arguments): 
-    hosp = arguments['--hosp']
-    data = arguments['--data']
-
-    if data != None:
-        COORDINATES = data
+def main(hosp, randon, qnt_point): 
+    SCALA = 100
+    if randon:
+        COORDINATES = np.random.rand(int(qnt_point), 2)*SCALA
     else:
-        COORDINATES = HOSPITAL if hosp == "hospital" else FULL_HOSPITAL
+        COORDINATES = HOSPITAL if hosp else FULL_HOSPITAL
         
     
     diagrams_of_voronoi(COORDINATES)
@@ -32,4 +32,9 @@ def main(arguments):
 
 if __name__ == "__main__":
     arguments = docopt(__doc__)
-    main(arguments)
+    
+    hosp = arguments['--hosp']
+    randon = arguments['--randon']
+    qnt_point = arguments['--qnt_point'] if arguments['--qnt_point'] else 10
+    
+    main(hosp, randon, qnt_point)
