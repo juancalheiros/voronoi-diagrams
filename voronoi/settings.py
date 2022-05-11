64c4e04a0e1e60ec35e0069e1a79f0d2b60f89ee
Juan Calheiros <juan.calheiros@g.globo>
2022-05-11 17:40:14 -0300

    fix: added coordanates of fiocruz

--- voronoi/settings.py
+++ voronoi/settings.py
@@ -15,7 +15,7 @@ hosp_camp_riocentro=[66.2843, 745.7806]
 hosp_camp_parque_atletas=[66.3299, 745.7940]
 
 
-FULL_HOSPITAL = os.environ.get('FULL_HOSPITAL', [ hosp_acari, hosp_salgado_filho, hosp_fundao, hosp_bonsucesso, hosp_jesus, hosp_pedro_ernesto, hosp_camp_maracana, hosp_souza_aguiar, hosp_miguel_couto, hosp_camp_leblon, hosp_camp_riocentro, hosp_camp_parque_atletas])
+FULL_HOSPITAL = os.environ.get('FULL_HOSPITAL', [ hosp_acari, hosp_salgado_filho, hosp_fundao, hosp_bonsucesso, hosp_jesus, hosp_pedro_ernesto, hosp_camp_maracana, hosp_souza_aguiar,fiocrus, hosp_miguel_couto, hosp_camp_leblon, hosp_camp_riocentro, hosp_camp_parque_atletas])
 
 HOSPITAL = os.environ.get('HOSPITAL', [ hosp_acari, hosp_salgado_filho, hosp_fundao, hosp_bonsucesso, hosp_jesus, hosp_pedro_ernesto, hosp_souza_aguiar, hosp_miguel_couto])
 
